7e84a265f5c6914244ecfc2edcacf0be8e51b273
Tomasz Kielar <git@k1eu.dev>
2024-06-07 15:47:28 +0200

    chore: update react & add tailwindcss

--- examples/common_nestjs_remix/apps/web/app/modules/Dashboard/Dashboard.layout.tsx
+++ examples/common_nestjs_remix/apps/web/app/modules/Dashboard/Dashboard.layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "@remix-run/react";
 export default function DashboardLayout() {
   return (
     <div>
-      <h1>Dashboard Layout lol</h1>
+      <h1>Dashboard Layout</h1>
       <Outlet />
     </div>
   );
