976b8aa064a150dde674c5f7366fc3138219b568
Tomasz Kielar <git@k1eu.dev>
2024-07-05 13:37:13 +0200

    feat(boilerplate): add auth guard in webapp

---
+++ examples/common_nestjs_remix/apps/web/app/modules/Auth/authGuard.ts
@@ -0,0 +1,15 @@
+import { redirect } from "@remix-run/react";
+import { useAuthStore } from "./authStore";
+
+/**
+ * @returns void
+ * @description This function is used to rediect the user to the login page if they are not logged in.
+ * @description Should be moved to middlewares once they are supported in Remix/ReactRouter7
+ */
+export function authGuard() {
+  const authStore = useAuthStore.getState();
+
+  if (!authStore.isLoggedIn) {
+    return redirect("/login");
+  }
+}
