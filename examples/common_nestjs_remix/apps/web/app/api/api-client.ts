fdd99bff36286f456cf60f489ddef238f929ab62
Jakub Wilk <jwilk436@gmail.com>
2024-07-16 16:51:26 +0200

    feat: add AuthLayout and RegisterPage components

--- examples/common_nestjs_remix/apps/web/app/api/api-client.ts
+++ examples/common_nestjs_remix/apps/web/app/api/api-client.ts
@@ -3,4 +3,5 @@ import { API } from "./generated-api";
 export const ApiClient = new API({
   baseURL: import.meta.env.API_URL,
   secure: true,
+  withCredentials: true,
 });
---
+++ examples/common_nestjs_remix/apps/web/app/modules/Auth/Register.page.tsx
@@ -0,0 +1,50 @@
+import { Link } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+export default function RegisterPage() {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="user@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Create an account
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link to="/auth/login" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
