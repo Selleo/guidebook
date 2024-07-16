95144cc7250192c1d62148abc5552e6433838904
Jakub Wilk <jwilk436@gmail.com>
2024-07-16 16:51:25 +0200

    feat: create Card components and update utility usage

--- examples/common_nestjs_remix/apps/web/vite.config.ts
+++ examples/common_nestjs_remix/apps/web/vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { routes } from "./routes";
+import path from "path";
 
 export default defineConfig({
   plugins: [
