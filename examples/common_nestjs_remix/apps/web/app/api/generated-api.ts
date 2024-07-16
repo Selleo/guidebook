9faae2117df97c35fea201ffffbd5d05100bc16e
Jakub Wilk <jwilk436@gmail.com>
2024-07-16 16:51:26 +0200

    feat: update API and generated API for authentication

--- examples/common_nestjs_remix/apps/web/app/api/generated-api.ts
+++ examples/common_nestjs_remix/apps/web/app/api/generated-api.ts
@@ -9,22 +9,91 @@
  * ---------------------------------------------------------------
  */
 
-export interface CreatePropertyBody {
-  name: string;
-  description?: string;
+export interface RegisterBody {
+  /** @format email */
+  email: string;
+  /**
+   * @minLength 8
+   * @maxLength 64
+   */
+  password: string;
 }
 
-export interface CreatePropertyResponse {
+export interface RegisterResponse {
   data: {
     id: string;
     createdAt: string;
     updatedAt: string;
-    name: string;
-    description: string | null;
+    email: string;
   };
 }
 
-export type DeletePropertyResponse = null;
+export interface LoginBody {
+  /** @format email */
+  email: string;
+  /**
+   * @minLength 8
+   * @maxLength 64
+   */
+  password: string;
+}
+
+export interface LoginResponse {
+  data: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    email: string;
+  };
+}
+
+export type LogoutResponse = null;
+
+export type RefreshTokensResponse = null;
+
+export interface GetUsersResponse {
+  data: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    email: string;
+  }[];
+}
+
+export interface GetUserByIdResponse {
+  data: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    email: string;
+  };
+}
+
+export interface UpdateUserBody {
+  /** @format email */
+  email?: string;
+}
+
+export interface UpdateUserResponse {
+  data: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    email: string;
+  };
+}
+
+export interface ChangePasswordBody {
+  /**
+   * @minLength 8
+   * @maxLength 64
+   */
+  password: string;
+}
+
+export type ChangePasswordResponse = null;
+
+export type DeleteUserResponse = null;
 
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse, HeadersDefaults, ResponseType } from "axios";
 import axios from "axios";
@@ -168,16 +237,16 @@ export class HttpClient<SecurityDataType = unknown> {
  * Example usage of Swagger with Typebox
  */
 export class API<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
-  properties = {
+  auth = {
     /**
      * No description
      *
-     * @name PropertiesControllerCreateProperty
-     * @request POST:/properties
+     * @name AuthControllerRegister
+     * @request POST:/auth/register
      */
-    propertiesControllerCreateProperty: (data: CreatePropertyBody, params: RequestParams = {}) =>
-      this.request<CreatePropertyResponse, any>({
-        path: `/properties`,
+    authControllerRegister: (data: RegisterBody, params: RequestParams = {}) =>
+      this.request<RegisterResponse, any>({
+        path: `/auth/register`,
         method: "POST",
         body: data,
         type: ContentType.Json,
@@ -188,13 +257,118 @@ export class API<SecurityDataType extends unknown> extends HttpClient<SecurityDa
     /**
      * No description
      *
-     * @name PropertiesControllerDeleteProperty
-     * @request DELETE:/properties/{id}
+     * @name AuthControllerLogin
+     * @request POST:/auth/login
      */
-    propertiesControllerDeleteProperty: (id: string, params: RequestParams = {}) =>
-      this.request<DeletePropertyResponse, any>({
-        path: `/properties/${id}`,
+    authControllerLogin: (data: LoginBody, params: RequestParams = {}) =>
+      this.request<LoginResponse, any>({
+        path: `/auth/login`,
+        method: "POST",
+        body: data,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name AuthControllerLogout
+     * @request POST:/auth/logout
+     */
+    authControllerLogout: (params: RequestParams = {}) =>
+      this.request<LogoutResponse, any>({
+        path: `/auth/logout`,
+        method: "POST",
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name AuthControllerRefreshTokens
+     * @request POST:/auth/refresh
+     */
+    authControllerRefreshTokens: (params: RequestParams = {}) =>
+      this.request<RefreshTokensResponse, any>({
+        path: `/auth/refresh`,
+        method: "POST",
+        format: "json",
+        ...params,
+      }),
+  };
+  users = {
+    /**
+     * No description
+     *
+     * @name UsersControllerGetUsers
+     * @request GET:/users
+     */
+    usersControllerGetUsers: (params: RequestParams = {}) =>
+      this.request<GetUsersResponse, any>({
+        path: `/users`,
+        method: "GET",
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name UsersControllerGetUserById
+     * @request GET:/users/{id}
+     */
+    usersControllerGetUserById: (id: string, params: RequestParams = {}) =>
+      this.request<GetUserByIdResponse, any>({
+        path: `/users/${id}`,
+        method: "GET",
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name UsersControllerUpdateUser
+     * @request PATCH:/users/{id}
+     */
+    usersControllerUpdateUser: (id: string, data: UpdateUserBody, params: RequestParams = {}) =>
+      this.request<UpdateUserResponse, any>({
+        path: `/users/${id}`,
+        method: "PATCH",
+        body: data,
+        type: ContentType.Json,
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name UsersControllerDeleteUser
+     * @request DELETE:/users/{id}
+     */
+    usersControllerDeleteUser: (id: string, params: RequestParams = {}) =>
+      this.request<DeleteUserResponse, any>({
+        path: `/users/${id}`,
         method: "DELETE",
+        format: "json",
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @name UsersControllerChangePassword
+     * @request PATCH:/users/{id}/change-password
+     */
+    usersControllerChangePassword: (id: string, data: ChangePasswordBody, params: RequestParams = {}) =>
+      this.request<ChangePasswordResponse, any>({
+        path: `/users/${id}/change-password`,
+        method: "PATCH",
+        body: data,
+        type: ContentType.Json,
         format: "json",
         ...params,
       }),
