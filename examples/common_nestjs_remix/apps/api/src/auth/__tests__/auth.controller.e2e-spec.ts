3f5015262b041dbeab1582a839bcf306874a422b
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2024-07-17 14:09:10 +0200

    --wip-- [skip ci]

--- examples/common_nestjs_remix/apps/api/src/auth/__tests__/auth.controller.e2e-spec.ts
+++ examples/common_nestjs_remix/apps/api/src/auth/__tests__/auth.controller.e2e-spec.ts
@@ -1,67 +1,80 @@
 import { INestApplication } from "@nestjs/common";
 import { isArray } from "lodash";
 import request from "supertest";
-import { userWithCredentialFactory } from "../../../test/factory/user.factory";
+import { createUserFactory } from "../../../test/factory/user.factory";
 import { createE2ETest } from "../../../test/create-e2e-test";
 import { AuthService } from "../auth.service";
+import { DatabasePg } from "src/common";
 
 describe("AuthController (e2e)", () => {
   let app: INestApplication;
   let authService: AuthService;
+  let db: DatabasePg;
+  let userFactory: ReturnType<typeof createUserFactory>;
 
   beforeAll(async () => {
     const { app: testApp, getService } = await createE2ETest();
     app = testApp;
     authService = getService(AuthService);
+    db = app.get("DB");
+    userFactory = createUserFactory(db);
   });
 
-  describe("POST /auth/register", () => {
-    it("should register a new user", async () => {
-      const { user: newUser, credential: newCredential } =
-        userWithCredentialFactory.build();
-
-      const response = await request(app.getHttpServer())
-        .post("/auth/register")
-        .set("Accept", "application/json")
-        .set("Content-Type", "application/json")
-        .send({
-          email: newUser.email,
-          password: newCredential.password,
-        });
-
-      expect(response.status).toEqual(201);
-      expect(response.body.data).toHaveProperty("id");
-      expect(response.body.data.email).toBe(newUser.email);
-    });
-
-    it("should return 409 if user already exists", async () => {
-      const existingUser = {
-        email: "existing@example.com",
-        password: "password123",
-      };
-
-      await authService.register(existingUser.email, existingUser.password);
-
-      await request(app.getHttpServer())
-        .post("/auth/register")
-        .send(existingUser)
-        .expect(409);
-    });
-  });
+  // describe("POST /auth/register", () => {
+  //   it("should register a new user", async () => {
+  //     const { user: newUser, credential: newCredential } =
+  //       userWithCredentialFactory.build();
+  //
+  //     const response = await request(app.getHttpServer())
+  //       .post("/auth/register")
+  //       .set("Accept", "application/json")
+  //       .set("Content-Type", "application/json")
+  //       .send({
+  //         email: newUser.email,
+  //         password: newCredential.password,
+  //       });
+  //
+  //     expect(response.status).toEqual(201);
+  //     expect(response.body.data).toHaveProperty("id");
+  //     expect(response.body.data.email).toBe(newUser.email);
+  //   });
+  //
+  //   it("should return 409 if user already exists", async () => {
+  //     const existingUser = {
+  //       email: "existing@example.com",
+  //       password: "password123",
+  //     };
+  //
+  //     await authService.register(existingUser.email, existingUser.password);
+  //
+  //     await request(app.getHttpServer())
+  //       .post("/auth/register")
+  //       .send(existingUser)
+  //       .expect(409);
+  //   });
+  // });
 
   describe("POST /auth/login", () => {
     it("should login and return user data with cookies", async () => {
-      const { user, credential } = await userWithCredentialFactory
-        .transient({ saveToDatabase: true, authService })
-        .build();
+      const user = await userFactory
+        .withCredentials({
+          password: "password123",
+        })
+        .create({
+          email: "test@example.com",
+        });
 
-      console.log({ user, credential });
+      // const { user, credential } = await userWithCredentialFactory
+      //   .transient({ saveToDatabase: true, authService })
+      //   .build();
+
+      // console.log({ user, credential });
 
       const response = await request(app.getHttpServer())
         .post("/auth/login")
         .send({
           email: user.email,
-          password: credential.password,
+          password: user.credentials?.password,
         })
         .expect(201);
 
@@ -71,104 +84,104 @@ describe("AuthController (e2e)", () => {
       expect(response.headers["set-cookie"].length).toBe(2);
     });
 
-    it("should return 401 for invalid credentials", async () => {
-      await request(app.getHttpServer())
-        .post("/auth/login")
-        .send({
-          email: "wrong@example.com",
-          password: "wrongpassword",
-        })
-        .expect(401);
-    });
-  });
-
-  describe("POST /auth/logout", () => {
-    it("should clear token cookies for a logged-in user", async () => {
-      const { user } = userWithCredentialFactory.build();
-      const password = "password123";
-      await authService.register(user.email, password);
-
-      const loginResponse = await request(app.getHttpServer())
-        .post("/auth/login")
-        .send({
-          email: user.email,
-          password: password,
-        })
-        .expect(201);
-
-      const cookies = loginResponse.headers["set-cookie"];
-
-      let accessToken = "";
-      let refreshToken = "";
-
-      if (isArray(cookies)) {
-        /**
-         * https://www.npmjs.com/package/cookie
-maybe something like this? seems usefull
-         */
-        cookies.forEach((cookie) => {
-          if (cookie.startsWith("access_token=")) {
-            accessToken = cookie;
-          } else if (cookie.startsWith("refresh_token=")) {
-            refreshToken = cookie;
-          }
-        });
-      }
-
-      const logoutResponse = await request(app.getHttpServer())
-        .post("/auth/logout")
-        .set("Cookie", [accessToken, refreshToken].filter(Boolean))
-        .expect(201);
-
-      expect(logoutResponse.headers["set-cookie"]).toBeDefined();
-      const logoutCookies = logoutResponse.headers["set-cookie"];
-
-      expect(logoutCookies.length).toBe(2);
-      expect(logoutCookies[0]).toContain("access_token=;");
-      expect(logoutCookies[1]).toContain("refresh_token=;");
-    });
-  });
-
-  describe("POST /auth/refresh", () => {
-    it("should refresh tokens", async () => {
-      const { user } = userWithCredentialFactory.build();
-      const password = "password123";
-      await authService.register(user.email, password);
-
-      const loginResponse = await request(app.getHttpServer())
-        .post("/auth/login")
-        .send({
-          email: user.email,
-          password: password,
-        })
-        .expect(201);
-
-      const cookies = loginResponse.headers["set-cookie"];
-
-      let refreshToken = "";
-
-      if (isArray(cookies)) {
-        cookies.forEach((cookie) => {
-          if (cookie.startsWith("refresh_token=")) {
-            refreshToken = cookie;
-          }
-        });
-      }
-
-      const response = await request(app.getHttpServer())
-        .post("/auth/refresh")
-        .set("Cookie", [refreshToken])
-        .expect(201);
-
-      expect(response.headers["set-cookie"]).toBeDefined();
-      expect(response.headers["set-cookie"].length).toBe(2);
-    });
-
-    it("should return 401 for invalid refresh token", async () => {
-      await request(app.getHttpServer())
-        .post("/auth/refresh")
-        .set("Cookie", ["refreshToken=invalid_token"])
-        .expect(401);
-    });
+//     it("should return 401 for invalid credentials", async () => {
+//       await request(app.getHttpServer())
+//         .post("/auth/login")
+//         .send({
+//           email: "wrong@example.com",
+//           password: "wrongpassword",
+//         })
+//         .expect(401);
+//     });
+//   });
+//
+//   describe("POST /auth/logout", () => {
+//     it("should clear token cookies for a logged-in user", async () => {
+//       const { user } = userWithCredentialFactory.build();
+//       const password = "password123";
+//       await authService.register(user.email, password);
+//
+//       const loginResponse = await request(app.getHttpServer())
+//         .post("/auth/login")
+//         .send({
+//           email: user.email,
+//           password: password,
+//         })
+//         .expect(201);
+//
+//       const cookies = loginResponse.headers["set-cookie"];
+//
+//       let accessToken = "";
+//       let refreshToken = "";
+//
+//       if (isArray(cookies)) {
+//         /**
+//          * https://www.npmjs.com/package/cookie
+// maybe something like this? seems usefull
+//          */
+//         cookies.forEach((cookie) => {
+//           if (cookie.startsWith("access_token=")) {
+//             accessToken = cookie;
+//           } else if (cookie.startsWith("refresh_token=")) {
+//             refreshToken = cookie;
+//           }
+//         });
+//       }
+//
+//       const logoutResponse = await request(app.getHttpServer())
+//         .post("/auth/logout")
+//         .set("Cookie", [accessToken, refreshToken].filter(Boolean))
+//         .expect(201);
+//
+//       expect(logoutResponse.headers["set-cookie"]).toBeDefined();
+//       const logoutCookies = logoutResponse.headers["set-cookie"];
+//
+//       expect(logoutCookies.length).toBe(2);
+//       expect(logoutCookies[0]).toContain("access_token=;");
+//       expect(logoutCookies[1]).toContain("refresh_token=;");
+//     });
+//   });
+//
+//   describe("POST /auth/refresh", () => {
+//     it("should refresh tokens", async () => {
+//       const { user } = userWithCredentialFactory.build();
+//       const password = "password123";
+//       await authService.register(user.email, password);
+//
+//       const loginResponse = await request(app.getHttpServer())
+//         .post("/auth/login")
+//         .send({
+//           email: user.email,
+//           password: password,
+//         })
+//         .expect(201);
+//
+//       const cookies = loginResponse.headers["set-cookie"];
+//
+//       let refreshToken = "";
+//
+//       if (isArray(cookies)) {
+//         cookies.forEach((cookie) => {
+//           if (cookie.startsWith("refresh_token=")) {
+//             refreshToken = cookie;
+//           }
+//         });
+//       }
+//
+//       const response = await request(app.getHttpServer())
+//         .post("/auth/refresh")
+//         .set("Cookie", [refreshToken])
+//         .expect(201);
+//
+//       expect(response.headers["set-cookie"]).toBeDefined();
+//       expect(response.headers["set-cookie"].length).toBe(2);
+//     });
+//
+//     it("should return 401 for invalid refresh token", async () => {
+//       await request(app.getHttpServer())
+//         .post("/auth/refresh")
+//         .set("Cookie", ["refreshToken=invalid_token"])
+//         .expect(401);
+//     });
   });
 });
--- examples/common_nestjs_remix/apps/api/test/factory/user.factory.ts
+++ examples/common_nestjs_remix/apps/api/test/factory/user.factory.ts
@@ -1,18 +1,14 @@
 import { faker } from "@faker-js/faker";
-import { InferSelectModel } from "drizzle-orm";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 import { Factory } from "fishery";
-import { AuthService } from "../../src/auth/auth.service";
+// import { AuthService } from "../../src/auth/auth.service";
 import { credentials, users } from "../../src/storage/schema";
+import { DatabasePg } from "src/common";
+import * as bcrypt from "bcrypt";
 
 type User = InferSelectModel<typeof users>;
-type Credential = InferSelectModel<typeof credentials>;
-
-export const userFactory = Factory.define<User>(() => ({
-  id: faker.string.uuid(),
-  email: faker.internet.email(),
-  createdAt: new Date().toISOString(),
-  updatedAt: new Date().toISOString(),
-}));
+type UserWithCredentials = User & { credentials?: Credential };
+type Credential = InferInsertModel<typeof credentials>;
 
 export const credentialFactory = Factory.define<Credential>(() => ({
   id: faker.string.uuid(),
@@ -22,23 +18,82 @@ export const credentialFactory = Factory.define<Credential>(() => ({
   updatedAt: new Date().toISOString(),
 }));
 
-type UserWithCredential = {
-  user: User;
-  credential: Credential;
-};
+class UserFactory extends Factory<UserWithCredentials> {
+  withCredentials(credential: { password: string }) {
+    return this.associations({
+      credentials: credentialFactory.build(credential),
+    });
+  }
+}
 
-export const userWithCredentialFactory = Factory.define<UserWithCredential>(
-  ({ onCreate, transientParams }) => {
-    const user = userFactory.build();
-    const credential = credentialFactory.build({ userId: user.id });
-
+export const createUserFactory = (db: DatabasePg) => {
+  return UserFactory.define(({ onCreate, associations }) => {
     onCreate(async (user) => {
-      return authService.register(user.user.email, user.credential.password);
+      const [inserted] = await db.insert(users).values(user).returning();
+
+      if (associations.credentials && !associations.credentials.id) {
+        const [insertedCredential] = await db
+          .insert(credentials)
+          .values({
+            ...associations.credentials,
+            password: await bcrypt.hash(associations.credentials.password, 10),
+            userId: inserted.id,
+          })
+          .returning();
+
+        return {
+          ...inserted,
+          credentials: {
+            ...insertedCredential,
+            password: associations.credentials.password,
+          },
+        };
+      }
+
+      return inserted;
     });
 
     return {
-      user,
-      credential,
+      id: faker.string.uuid(),
+      email: faker.internet.email(),
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
     };
-  },
-);
+  });
+};
+
+// export const userFactory = Factory.define<User>(() => ({
+//   id: faker.string.uuid(),
+//   email: faker.internet.email(),
+//   createdAt: new Date().toISOString(),
+//   updatedAt: new Date().toISOString(),
+// }));
+//
+// export const credentialFactory = Factory.define<Credential>(() => ({
+//   id: faker.string.uuid(),
+//   userId: faker.string.uuid(),
+//   password: faker.internet.password(),
+//   createdAt: new Date().toISOString(),
+//   updatedAt: new Date().toISOString(),
+// }));
+//
+// type UserWithCredential = {
+//   user: User;
+//   credential: Credential;
+// };
+//
+// export const userWithCredentialFactory = Factory.define<UserWithCredential>(
+//   ({ onCreate, transientParams }) => {
+//     const user = userFactory.build();
+//     const credential = credentialFactory.build({ userId: user.id });
+//
+//     onCreate(async (user) => {
+//       return authService.register(user.user.email, user.credential.password);
+//     });
+//
+//     return {
+//       user,
+//       credential,
+//     };
+//   },
+// );
