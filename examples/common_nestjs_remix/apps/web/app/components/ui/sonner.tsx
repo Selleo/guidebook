070b41a8d55e3bfe4360cdff9b92be8a725e7992
Jakub Wilk <jwilk436@gmail.com>
2024-07-19 16:07:36 +0200

    refactor: update Toaster component and useTheme usage

--- examples/common_nestjs_remix/apps/web/app/components/ui/sonner.tsx
+++ examples/common_nestjs_remix/apps/web/app/components/ui/sonner.tsx
@@ -1,14 +1,14 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { Toaster as Sonner } from "sonner";
+import { useThemeStore } from "~/modules/Theme/themeStore";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme } = useThemeStore();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       toastOptions={{
         classNames: {
@@ -23,7 +23,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
