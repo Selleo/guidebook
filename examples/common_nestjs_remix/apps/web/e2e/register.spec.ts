67951a5c76880d47dbc616a16d2d71f18cf4dae9
Jakub Wilk <jwilk436@gmail.com>
2024-08-02 00:56:48 +0200

    feat: update e2e tests with new credentials and assertions

--- examples/common_nestjs_remix/apps/web/e2e/register.spec.ts
+++ examples/common_nestjs_remix/apps/web/e2e/register.spec.ts
@@ -1,17 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test("move to register page", async ({ page }) => {
-  await page.goto("/auth/login");
+test("fail on invalid credentials", async ({ page }) => {
+  await page.goto("/auth/register");
 
-  await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
-});
+  await page.getByLabel("email").fill("user@example");
+  await page.getByLabel("password").fill("pass");
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await page.getByRole("button", { name: /create/i }).click();
 
-  await page.getByRole("link", { name: "Get started" }).click();
-
+  await expect(page.getByText("Invalid email")).toBeVisible();
   await expect(
-    page.getByRole("heading", { name: "Installation" })
+    page.getByText("Password must be at least 8 characters")
   ).toBeVisible();
 });
