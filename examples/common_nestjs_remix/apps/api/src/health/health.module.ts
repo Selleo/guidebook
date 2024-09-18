28110c086d64ef874ed28951dc8916d6e6f9bdd2
Dawid Loranc <dawidloranc@gmail.com>
2024-09-18 14:58:22 +0200

    feat: add DrizzleOrmHealthIndicator

--- examples/common_nestjs_remix/apps/api/src/health/health.module.ts
+++ examples/common_nestjs_remix/apps/api/src/health/health.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { TerminusModule } from "@nestjs/terminus";
 import { HttpModule } from "@nestjs/axios";
 import { HealthController } from "./health.controller";
+import { DrizzleOrmHealthIndicator } from "./indicator/database/drizzleorm.health";
 
 @Module({
   imports: [TerminusModule, HttpModule],
+  providers: [DrizzleOrmHealthIndicator],
   controllers: [HealthController],
 })
 export class HealthModule {}
