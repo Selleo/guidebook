0204f6af66a755f56b0f97ac6ee352e56d703c85
Konrad Pazgan <hello@konradpazgan.pl>
2024-10-07 11:28:01 +0200

    remove db changes

--- examples/common_nestjs_remix/apps/api/src/storage/migrations/0004_create_files.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "files" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"created_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	"updated_at" timestamp(3) with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	"filename" text NOT NULL,
-	"url" text NOT NULL,
-	"mimetype" text NOT NULL,
-	"size" integer NOT NULL
-);
